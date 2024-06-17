3faffdf0d65e2fa9ef1fe20db3a4aae0121fe393
Ubuntu <ubuntu@dev.cromlec.cat>
2024-06-17 15:36:04 +0000

    modificacions ReadMe

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,17 +5,11 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-use App\Models\Menugrup;
+
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
 
-     public function getmenu()
-    {
-
-        $menu=Menugrup::where("active",1)->orderby('order','asc')->get();
-
-        return $menu;
-    }
+    
 }
